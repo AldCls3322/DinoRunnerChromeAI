eb245cb437359c9329999fab51605ea08765b84d
AldCls3322 <a01423328@itesm.mx>
2021-03-13 22:39:40 -0600

    added a bird obstacle

--- Game/DinoGame.py
+++ Game/DinoGame.py
@@ -21,7 +21,7 @@ BLACK = (0,0,0)
 # S_CACTUS_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","SmallCactus01.png")))
 # B_CACTUS_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","BigCactus01.png")))
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","bird01.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","bird02.png")))]
+BIRD_IMGS = [pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","bird01.png")),(23, 20) )), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","bird02.png")),(23,20) ))]
 DINO_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","dino01.png"))), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","dinoRun01.png")),(24,28))), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","dinoRun02.png")),(24,28))), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","dinoDuck01Fixed.png")),(34,28))), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","dinoDuck02Fixed.png")),(34,28))), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","dinoJump01.png")),(24,25))), pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","dinoDeath01.png")))]
 CACTUS_IMGS = [pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","BigCactus01.png")),(15,30) )), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","SmallCactus01.png")),(10,20) )), pygame.transform.scale2x(pygame.transform.scale(pygame.image.load(os.path.join("Game/IMGS","ManySmallCactus01.png")), (30,20) ))]
 GROUND_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","ground.png")))
@@ -31,18 +31,19 @@ CLOUD_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS",
 class Bird:
     ##_CONSTANT VARIABLES USED_##
     IMGS = BIRD_IMGS
-    ANIMATION_TIME = 5
+    ANIMATION_TIME = 3
+    VEL = 10
 
     #_ESTABLISHED THE ATRIBUTES OF THE OBJECT BIRD | CONSTRUCTOR_#
-    def __init__(self, x, y):
+    def __init__(self, x):
         self.x = x
-        self.y = y
-        self.tilt = 0
-        self.tick_count = 0
-        self.vel = 0
-        self.height = self.y
+        self.y = random.randint(0,70)
         self.img_count = 0
         self.img = self.IMGS[0]
+    
+    #_FUNCTION TO MOVE THE PIPE IN X AXIS_#
+    def move(self):
+        self.x -= self.VEL  #Moves the pipe to the left
 
     #_FUNCTION TO CREATE ANIMATION_#    
     def draw(self, win):
@@ -54,18 +55,28 @@ class Bird:
             self.img = self.IMGS[0]
         elif self.img_count < self.ANIMATION_TIME*2:
             self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME*3:
-            self.img = self.IMGS[0]
-        elif self.img_count < self.ANIMATION_TIME*4:
-            self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+        elif self.img_count < self.ANIMATION_TIME*2 + 1:
             self.img = self.IMGS[0]
             self.img_count = 0
         
-    #_FUNCTION FOR COLISION_#
-    """ Uses the mask to be more acurrate for the user in colision, and not use boxes """
-    def get_mask(self):
-        return pygame.mask.from_surface(self.img)
+        win.blit(self.img, (self.x, self.y))
+        
+    #_COLITION USING MASK TO BE MORE ACCURATE WITH THE COLISION BOX_#
+    """ There will be colision box, however inside the colision box there is a mask (shaped like the bird, by checking if the background is transparent) 
+    so that although the boxes touch themselves, but the mask don't, then tere is no colision."""
+    def collide(self, dino, win):
+        dino_mask = dino.get_mask() #saves the positions of the dinosaur image
+        bird_mask = pygame.mask.from_surface(self.img)    #saves the obstacle position of the cactus
+
+        #checks if the masks collide
+        offset = (self.x - dino.x, self.y - round(dino.y))
+
+        point = dino_mask.overlap(bird_mask, offset)
+
+        if (point):
+            return True
+         
+        return False
 
 ###__Creates/Defines an object Base__###
 class Ground:
@@ -98,7 +109,7 @@ class Ground:
 
 ###__Creates/Defines an object Cloud__###
 class Cloud:
-    VEL = 7
+    VEL = 4
     IMG = CLOUD_IMG
 
     def __init__(self, x):
@@ -246,8 +257,8 @@ class Cactus:
         win.blit(self.img, (self.x, self.height))
     
     #_COLITION USING MASK TO BE MORE ACCURATE WITH THE COLISION BOX_#
-    """ There will be colision box, however inside the colision box there us a mask (shaped like the bird and the pipe, by checking if the background is transparent) 
-    son that although the boxes touch themselves, but the mask dn't, then tere is no colision."""
+    """ There will be colision box, however inside the colision box there is a mask (shaped like the cactus, by checking if the background is transparent) 
+    so that although the boxes touch themselves, but the mask don't, then tere is no colision."""
     def collide(self, dino, win):
         dino_mask = dino.get_mask() #saves the positions of the dinosaur image
         cactus_mask = pygame.mask.from_surface(self.img)    #saves the obstacle position of the cactus
@@ -268,7 +279,7 @@ class Cactus:
 
 ###__GAME FUNCTIONS__###
 #_FUNCTION THAT CREATES A SCREEEN_#
-def draw_window(screen, ground, clouds, dino, cacti):
+def draw_window(screen, ground, clouds, dino, birds, cacti):
     screen.fill(WHITE)
     ground.draw(screen) #Displays ground
     
@@ -276,6 +287,9 @@ def draw_window(screen, ground, clouds, dino, cacti):
         cloud.draw(screen)
 
     dino.draw(screen)   #Displays dinosaur
+
+    for bird in birds:
+        bird.draw(screen)   #Displays bird
     
     for cactus in cacti:
         cactus.draw(screen)
@@ -285,21 +299,25 @@ def draw_window(screen, ground, clouds, dino, cacti):
 def main():
     #_Creating Objects_#
     ground = Ground(125) #creates and sets the ground
-    clouds = [Cloud(600)]
+    clouds = [Cloud(600)]   # creates a list for the clouds
     dino = Dino(0,95)   #creates and setes the dinosaur
     #cactus = Cactus(550)   #creates a single cactus
-    cacti = [Cactus(550)]
+    birds = [Bird(900)]     # creates a single bird
+    cacti = [Cactus(550)]   # creates a list for multiple cactus to appear on screeen
 
+    #_Making the Screen and defining a Time_#
     screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT)) #sets the window size
-    screen.fill(WHITE)
+    screen.fill(WHITE)  # makes the background WHITE
     clock = pygame.time.Clock() #creates a control for frame rates
 
-    prev_cactus = 600
-    for i in range(2):
+    #_Makes the obstacle of continious and with random distance between them_#
+    prev_cactus = 600   # defines that the first cactus was set on x = 600
+    for i in range(2):  # creates 2 different cactuses with random distance between them and adds them to the previous created list
         prev_cactus += random.randint(300,600)
         cacti.append(Cactus(prev_cactus))
 
-    run = True
+    run = True  # SETS START TO THE GAME
+
     while run: #Plays game while you don't quit
         clock.tick(30)  #Sets the frame rate at 30
 
@@ -326,47 +344,62 @@ def main():
                 cactus.passed = True
                 add_cactus = True
 
-            if cactus.collide(dino, screen):
+            if cactus.collide(dino, screen):    # checks of the Dinosaur touched a Cactus
                 print('TOUCHED')
             
-            if (cactus.x < -30):
+            if (cactus.x < -30):    # after the cactus passes the screen it will be deleted, so we save it on a list that will later remove that cactus
                 rem_cactus.append(cactus)
             
-            cactus.move()
+            cactus.move()   # sets motion to tha cactuses
         
         
-        if (add_cactus):
+        if (add_cactus):    # if the dinosaur passed a cactus it will generate new ones with same method as done previously
             prev_cactus = cacti[len(cacti)-1].x
             prev_cactus += random.randint(200,600)
             cacti.append(Cactus(prev_cactus))
 
-        for re in rem_cactus:
+        for re in rem_cactus:   # deletes the cactus that was already jumped
             cacti.remove(re)
         
         
-        add_cloud = False
-        rem_cloud = []
-        for cloud in clouds:
-            if (cloud.x < -99):
+        add_cloud = False   # establishes not to add a cloud yet
+        rem_cloud = []      # a list to save the clouds that need to be removed
+        for cloud in clouds:    # will establish what happens to the clouds
+            if (cloud.x < -99): # after the cloud passes through the screen it will create a new one.
                 add_cloud = True
             
-            if (cloud.x < -100):
+            if (cloud.x < -100):    # after the cloud passes the screen it saves it to the list that will delete tha passed clouds
                 rem_cloud.append(cloud)
             
-            cloud.move()
+            cloud.move()    # sets motion to the clouds
 
-        if (add_cloud):
+        if (add_cloud): # if it must create a new cloud it will create a new one in the right side.
             clouds = [Cloud(600)]
             add_cloud = False
         
-        for r in rem_cloud:
+        for r in rem_cloud: # deletes the passed cloud
             clouds.remove(r)
         
+        add_bird = False
+        rem_bird = []
+        for bird in birds:
+            if (bird.x < -23):
+                add_bird = True
+                rem_bird.append(bird)
+            
+            bird.move() #Makes the bi rd move
+        
+        if (add_bird):  # after one passes the screen it creates a new one 
+            birds.append(Bird(1800))
+            add_bird = False
+        
+        for r in rem_bird:  # deletes the passed bird
+            birds.remove(r)
 
         ground.move()   #Makes the ground move to give a perseption of running
         dino.move()     #Makes the dinosaur constantly run
 
-        draw_window(screen, ground, clouds, dino, cacti) #calls the function "draw_window"
+        draw_window(screen, ground, clouds, dino, birds, cacti) #calls the function "draw_window"
 
 
 main()
