5919f649cebf6db28a26c5d1d9882b5b6df5a019
AldCls3322 <a01423328@itesm.mx>
2021-03-14 15:47:28 -0600

    added an ending screen and ability to restart

--- Game/DinoGame.py
+++ Game/DinoGame.py
@@ -12,9 +12,11 @@ WIN_HEIGHT = 150
 #_COLORS_#
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+GREY = (128,128,128)
 
 #_FONTS_#
 SCORE_FONT = pygame.font.SysFont("Rockwell", 24)    #"Speak Pro" "Source Sans Pro"
+ENDING_FONT = pygame.font.SysFont("Speak Pro", 30)
 
 #_IMAGES_#
 # DINO_RUN_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","dinoRun01.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("Game/IMGS","dinoRun02.png")))]
@@ -282,7 +284,7 @@ class Cactus:
 
 ###__GAME FUNCTIONS__###
 #_FUNCTION THAT CREATES A SCREEEN_#
-def draw_window(screen, ground, clouds, dino, birds, cacti, score):
+def draw_window(screen, ground, clouds, dino, birds, cacti, score, alive):
     screen.fill(WHITE)
     ground.draw(screen) #Displays ground
     
@@ -299,12 +301,17 @@ def draw_window(screen, ground, clouds, dino, birds, cacti, score):
 
     #Score Dispay#
     score_label = SCORE_FONT.render("Score: " + str(score),1,BLACK)
-    screen.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15,10))
+    screen.blit(score_label, (WIN_WIDTH - score_label.get_width() - 15,5))
+
+    #Ending Text Display#
+    if (alive == False):
+        ending_label = ENDING_FONT.render("Press 'BACKSPACE' to restart",1,GREY)
+        screen.blit(ending_label, (WIN_WIDTH/2 - ending_label.get_width()/2, WIN_HEIGHT/2))
 
     pygame.display.update() #refreshes
 
-#_FUCNTION THAT PALYS THE GAME_#
-def main():
+#_FUNCTION TO RESTART/START_#
+def restart():
     #_Creating Objects_#
     ground = Ground(125) #creates and sets the ground
     clouds = [Cloud(600)]   # creates a list for the clouds
@@ -327,6 +334,14 @@ def main():
         cacti.append(Cactus(prev_cactus))
 
     run = True  # SETS START TO THE GAME
+    alive = True
+
+    return ground, clouds, dino, birds, cacti, screen, clock, score, prev_cactus, run, alive
+
+#_FUCNTION THAT PALYS THE GAME_#
+def main():
+    
+    ground, clouds, dino, birds, cacti, screen, clock, score, prev_cactus, run, alive = restart()
 
     while run: #Plays game while you don't quit
         clock.tick(30)  #Sets the frame rate at 30
@@ -334,89 +349,96 @@ def main():
         for event in pygame.event.get():    #when something is clicked then:
             if event.type == pygame.QUIT:   #will quit the loop for the "X" button on the screen
                 run = False
-                #pygame.quit()
                 quit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     dino.jump()
-                # if event.key == pygame.K_DOWN:
-                #     dino.duck()
+                if event.key == pygame.K_BACKSPACE:
+                    print("RESTART")
+                    ground, clouds, dino, birds, cacti, screen, clock, score, prev_cactus, run, alive = restart()
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+                    quit()
+        
+        if (alive):
 
-        keys = pygame.key.get_pressed() #checks for a key of the keyboard is being pressed
-        if keys[pygame.K_DOWN]: #if the key is the down arrow, then it will
-            dino.duck() #makes dinosaur duck
+            keys = pygame.key.get_pressed() #checks for a key of the keyboard is being pressed
+            if keys[pygame.K_DOWN]: #if the key is the down arrow, then it will
+                dino.duck() #makes dinosaur duck
 
-        add_cactus = False  #establishes not to add a cactus
-        rem_cactus = []    #for saving which cactus to delete
-        for cactus in cacti:
-            
-            if (not cactus.passed and cactus.x < dino.x):   #until the dinosaur passes the cactus it will create another one
-                cactus.passed = True
-                add_cactus = True
+            add_cactus = False  #establishes not to add a cactus
+            rem_cactus = []    #for saving which cactus to delete
+            for cactus in cacti:
+                
+                if (not cactus.passed and cactus.x < dino.x):   #until the dinosaur passes the cactus it will create another one
+                    cactus.passed = True
+                    add_cactus = True
 
-            if cactus.collide(dino, screen):    # checks of the Dinosaur touched a Cactus
-                print('TOUCHED')
+                if cactus.collide(dino, screen):    # checks of the Dinosaur touched a Cactus
+                    print('TOUCHED')
+                    alive = False
+                
+                if (cactus.x < -30):    # after the cactus passes the screen it will be deleted, so we save it on a list that will later remove that cactus
+                    rem_cactus.append(cactus)
+                
+                cactus.move()   # sets motion to tha cactuses
             
-            if (cactus.x < -30):    # after the cactus passes the screen it will be deleted, so we save it on a list that will later remove that cactus
-                rem_cactus.append(cactus)
             
-            cactus.move()   # sets motion to tha cactuses
-        
-        
-        if (add_cactus):    # if the dinosaur passed a cactus it will generate new ones with same method as done previously
-            score += 1
+            if (add_cactus):    # if the dinosaur passed a cactus it will generate new ones with same method as done previously
+                score += 1
 
-            prev_cactus = cacti[len(cacti)-1].x
-            prev_cactus += random.randint(200,600)
-            cacti.append(Cactus(prev_cactus))
+                prev_cactus = cacti[len(cacti)-1].x
+                prev_cactus += random.randint(200,600)
+                cacti.append(Cactus(prev_cactus))
 
-        for re in rem_cactus:   # deletes the cactus that was already jumped
-            cacti.remove(re)
-        
-        
-        add_cloud = False   # establishes not to add a cloud yet
-        rem_cloud = []      # a list to save the clouds that need to be removed
-        for cloud in clouds:    # will establish what happens to the clouds
-            if (cloud.x < -99): # after the cloud passes through the screen it will create a new one.
-                add_cloud = True
+            for re in rem_cactus:   # deletes the cactus that was already jumped
+                cacti.remove(re)
             
-            if (cloud.x < -100):    # after the cloud passes the screen it saves it to the list that will delete tha passed clouds
-                rem_cloud.append(cloud)
             
-            cloud.move()    # sets motion to the clouds
+            add_cloud = False   # establishes not to add a cloud yet
+            rem_cloud = []      # a list to save the clouds that need to be removed
+            for cloud in clouds:    # will establish what happens to the clouds
+                if (cloud.x < -99): # after the cloud passes through the screen it will create a new one.
+                    add_cloud = True
+                
+                if (cloud.x < -100):    # after the cloud passes the screen it saves it to the list that will delete tha passed clouds
+                    rem_cloud.append(cloud)
+                
+                cloud.move()    # sets motion to the clouds
 
-        if (add_cloud): # if it must create a new cloud it will create a new one in the right side.
-            clouds = [Cloud(600)]
-            add_cloud = False
-        
-        for r in rem_cloud: # deletes the passed cloud
-            clouds.remove(r)
-        
-        add_bird = False
-        rem_bird = []
-        for bird in birds:
-            if (bird.x < -23):
-                add_bird = True
-                rem_bird.append(bird)
+            if (add_cloud): # if it must create a new cloud it will create a new one in the right side.
+                clouds = [Cloud(600)]
+                add_cloud = False
             
-            if bird.collide(dino, screen):    # checks of the Dinosaur touched a Bird
-                print('TOUCHED')
+            for r in rem_cloud: # deletes the passed cloud
+                clouds.remove(r)
             
-            bird.move() #Makes the bird move
-        
-        if (add_bird):  # after one passes the screen it creates a new one 
-            score += 1
-            
-            birds.append(Bird(1800))
             add_bird = False
-        
-        for r in rem_bird:  # deletes the passed bird
-            birds.remove(r)
+            rem_bird = []
+            for bird in birds:
+                if (bird.x < -23):
+                    add_bird = True
+                    rem_bird.append(bird)
+                
+                if bird.collide(dino, screen):    # checks of the Dinosaur touched a Bird
+                    print('TOUCHED')
+                    alive = False
+                
+                bird.move() #Makes the bird move
+            
+            if (add_bird):  # after one passes the screen it creates a new one 
+                score += 1
+                
+                birds.append(Bird(1800))
+                add_bird = False
+            
+            for r in rem_bird:  # deletes the passed bird
+                birds.remove(r)
 
-        ground.move()   #Makes the ground move to give a perseption of running
-        dino.move()     #Makes the dinosaur constantly run
+            ground.move()   #Makes the ground move to give a perseption of running
+            dino.move()     #Makes the dinosaur constantly run
 
-        draw_window(screen, ground, clouds, dino, birds, cacti, score) #calls the function "draw_window"
+        draw_window(screen, ground, clouds, dino, birds, cacti, score, alive) #calls the function "draw_window"
 
 
 main()
